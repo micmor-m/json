14e9da45ed61b816cce3bd3de96ea38920d5d9e8
mic <micmor.m@gmail>
2021-03-29 13:17:38 -0400

    Update functionality from query params to params

--- src/main/java/RequestProcessor.java
+++ src/main/java/RequestProcessor.java
@@ -30,40 +30,35 @@ class RequestProcessor implements Runnable {
     }
     // Create an object with the 3 keys-values pair from params
     public static Map<String, List<String>> getQueryParams(String url) {
-            String trimmedUrl = null;
-            Map<String, List<String>> params = new HashMap<String, List<String>>();
-            String[] urlParts = url.split(" ");
-            if (urlParts.length > 1) {
-                // remove "/?" at the beginning of the url parameters
-                trimmedUrl = urlParts[1].substring(2);
-                String query = trimmedUrl ;
-                // separate the key and value pair and put into the object
-                for (String param : query.split("&")) {
-                    String[] pair = param.split("=");
-                    String key = null;
-                    try {
-                        key = URLDecoder.decode(pair[0], "UTF-8");
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                    String value = "";
-                    if (pair.length > 1) {
-                        try {
-                            value = URLDecoder.decode(pair[1], "UTF-8");
-                        } catch (UnsupportedEncodingException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    List<String> values = params.get(key);
-
-                    if (values == null) {
-                        values = new ArrayList<String>();
-                        params.put(key, values);
-                    }
-                    values.add(value);
+        Map<String, List<String>> params = new HashMap<String, List<String>>();
+        String[] urlParts = url.split(" ");
+        if (urlParts.length > 1) {
+            String query = urlParts[0] ;
+            System.out.println(query);
+            // separate the key and value pair and put into the object
+            for (String param : query.split("&")) {
+                String[] pair = param.split("=");
+                String key = null;
+                try {
+                    key = URLDecoder.decode(pair[0], "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
+                String value = "";
+                if (pair.length > 1) {
+                    value = pair[1];
+                    System.out.println("Value" + pair[1]);
+                }
+                List<String> values = params.get(key);
+
+                if (values == null) {
+                    values = new ArrayList<String>();
+                    params.put(key, values);
+                }
+                values.add(value);
             }
-            return params;
+        }
+        return params;
     }
 
     public void run() {
@@ -75,46 +70,43 @@ class RequestProcessor implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-            System.out.println(url);
-            String[] urlRoute = url.split("\\?");
-            System.out.println(urlRoute[0]);
-            // Verify that the route requested from the client is the correct on "GET /"
-            if (!urlRoute[0].toUpperCase().equals("GET /")){
-                System.out.println("Client required wrong route");
+        // Discard first 5 characters for the route
+        String urlRoute = url.substring(5);
+
+        if (!url.substring(0,5).toUpperCase().equals("GET /")){
+            System.out.println("Client required wrong route");
+        } else {
+            Map<String, List<String>> params = this.getQueryParams(urlRoute);
+            if ((String.valueOf(params.get("leftOperand").get(0)) == "") || (String.valueOf(params.get("rightOperand").get(0)) == "") || (params.get("operation").get(0).equals("undefined"))) {
+                System.out.println("At least one of the required params was empty");
             } else {
-                Map<String, List<String>> params = this.getQueryParams(url);
+                if (isNumeric(String.valueOf(params.get("leftOperand").get(0))) && isNumeric(String.valueOf(params.get("rightOperand").get(0)))) {
+                    float leftNum = Float.parseFloat(String.valueOf(params.get("leftOperand").get(0)));
+                    float rightNum = Float.parseFloat(String.valueOf(params.get("rightOperand").get(0)));
+                    String operation = String.valueOf(params.get("operation").get(0));
+                    float result = 0;
 
-                if ((String.valueOf(params.get("leftOperand").get(0)) == "") || (String.valueOf(params.get("rightOperand").get(0)) == "") || (params.get("operation").get(0).equals("undefined"))) {
-                    System.out.println("At least one of the required params was empty");
-
-                } else {
-                    if (isNumeric(String.valueOf(params.get("leftOperand").get(0))) && isNumeric(String.valueOf(params.get("rightOperand").get(0)))) {
-                        float leftNum = Float.parseFloat(String.valueOf(params.get("leftOperand").get(0)));
-                        float rightNum = Float.parseFloat(String.valueOf(params.get("rightOperand").get(0)));
-                        String operation = String.valueOf(params.get("operation").get(0));
-                        float result = 0;
-
-                        if (operation.equals("+")) {
-                            result = leftNum + rightNum;
-                        } else if (operation.equals("-")) {
-                            result = leftNum - rightNum;
-                        } else if (operation.equals("*")) {
-                            result = leftNum * rightNum;
-                        } else if (operation.equals("/")) {
-                            result = leftNum / rightNum;
-                        } else if (operation.equals("%25")) {
-                            operation = "%";
-                            result = leftNum % rightNum;
-                        }
-                        // mock-up JSON object to send to client
-                        System.out.println(result);
-                        jsonObject.put("Expression", leftNum + " " + operation + " " + rightNum);
-                        jsonObject.put("Result", result);
-                    } else {
-                        System.out.println("At least one of the required params was not parsable");
+                    if (operation.equals("+")) {
+                        result = leftNum + rightNum;
+                    } else if (operation.equals("-")) {
+                        result = leftNum - rightNum;
+                    } else if (operation.equals("*")) {
+                        result = leftNum * rightNum;
+                    } else if (operation.equals("/")) {
+                        result = leftNum / rightNum;
+                    } else if (operation.equals("%25")) {
+                        operation = "%";
+                        result = leftNum % rightNum;
                     }
+                    // mock-up JSON object to send to client
+                    System.out.println(result);
+                    jsonObject.put("Expression", leftNum + " " + operation + " " + rightNum);
+                    jsonObject.put("Result", result);
+                } else {
+                    System.out.println("At least one of the required params was not parsable");
                 }
             }
+        }
 
 //end of your code
         String response = msgToClient + jsonObject.toString();
